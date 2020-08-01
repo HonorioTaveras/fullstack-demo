3ae47e0798d0c6dcbee2308ed8f7c94e7517c952
Honorio <htaveras90@gmail.com>
2020-08-01 11:09:45 -0600

    update queries.js, created schema, exp/imported

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,1 +1,28 @@
-// create db
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/bugs", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+var bugReportsSchema = mongoose.Schema({
+  bugName: String,
+  bugDescription: String,
+  reportedBy: String,
+  createdDate: String,
+  assignedTo: String,
+  threatLevel: String,
+});
+
+var BugReports = mongoose.model("BugReports", bugReportsSchema);
+
+const getAllBugReports = () => {
+
+}
+
+const addNewBugReport = () => {
+
+}
+module.exports = {
+  getAllBugReports,
+  addNewBugReport
+};
--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3333
+const port = 3333;
+const BugReports = require('./db/queries.js');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/bugs', (req, res) => {
-  res.send('hello bugs');
+app.get("/api/bugs", (req, res) => {
+  res.send("hello bugs");
 });
 
-app.post('/api/bugs', (req, res) => {
+app.post("/api/bugs", (req, res) => {});
 
-});
-
-app.listen(port, () => console.log(`listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
